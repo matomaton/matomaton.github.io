0a6b77176887b741ac094e925106f9efa665ab98
matomaton <matthew.martin@ithaka.org>
2024-01-09 06:59:10 -0500

    updated story

--- PLATFORM-STORY/storyscript.js
+++ PLATFORM-STORY/storyscript.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const imageStack = document.getElementById("imageStack");
 
-    for (let i = 4; i <= 56; i++) {
+    for (let i = 4; i <= 21; i++) {
         const img = document.createElement("img");
         img.src = `https://matomaton.github.io/assets/JSTOR%20platform%20story/${String(i).padStart(2, '0')}.png`;
         img.alt = `Image ${i}`;
