87b64e491f65ba88e4d35452ec01e9799c90c84f
matomaton <matthew.martin@ithaka.org>
2024-01-08 19:52:53 -0500

    added platform story page

---
+++ PLATFORM-STORY/storyscript.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const imageStack = document.getElementById("imageStack");
+
+    for (let i = 4; i <= 56; i++) {
+        const img = document.createElement("img");
+        img.src = `https://matomaton.github.io/assets/JSTOR%20platform%20story/${String(i).padStart(2, '0')}.png`;
+        img.alt = `Image ${i}`;
+        imageStack.appendChild(img);
+    }
+});
+
