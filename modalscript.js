abd5e834514aa349d130472ba92658fc1e9f80a3
matomaton <matthew.martin.atomaton@gmail.com>
2025-01-07 16:40:02 -0500

    adding modal files

---
+++ modalscript.js
@@ -0,0 +1,17 @@
+ const modal = document.getElementById("formModal");
+        const btn = document.getElementById("openModal");
+        const span = document.getElementsByClassName("close")[0];
+
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
